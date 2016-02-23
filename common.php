e3420b295affcee0e58cfd5ed7543c84d2f71eee
arezki <a.ibsaine@hotmail.fr>
2016-02-23 18:12:11 +0000

    the start

--- common.php
+++ common.php
@@ -1,10 +1,11 @@
 <?php 
 
     // These variables define the connection information for your MySQL database 
-    $username = "root"; 
-    $password = ""; 
-    $host = "localhost"; 
-    $dbname = "dbusername"; 
+    $username = "sql2107364"; 
+    $password = "dZ2*wT7%"; 
+    $host = "sql2.freemysqlhosting.net"; 
+    $dbname = "sql2107364"; 
+
 
     // UTF-8 is a character encoding scheme that allows you to conveniently store 
     // a wide varienty of special characters, like ¢ or €, in your database. 
