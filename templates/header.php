6abb0119dc3d31aa52c1c588d1520c25d7f2a241
maurams <maurice.ramsbottom@gmail.com>
2016-04-17 18:33:57 +0000

    fixed mini menu

--- templates/header.php
+++ templates/header.php
@@ -21,10 +21,13 @@
 <html>
  
 <head>
-	
-	 <link rel="stylesheet" href="css/style.css" type="text/css" />
-    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+    <!--needs to be at the top of the head to work-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <!--needed the secure cdn links for mini menu-->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet"/>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../css/style.css" type="text/css" />
+    <link rel="stylesheet" href="../css/bootstrap.css" type="text/css" />
     <link rel="stylesheet" href="../css/style.css" type="text/css" />
 
 
