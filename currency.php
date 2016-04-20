0e2af90effbb0a086fed0da41721b60f89118ea8
arezki <a.ibsaine@hotmail.fr>
2016-04-20 17:01:59 +0000

    currency

--- currency.php
+++ currency.php
@@ -32,24 +32,37 @@
 		<select name="from">
 			<option value="usd">USD</option>
 			<option value="pound">POUND</option>
+			<option value="euro">EURO</option>
+			<option value="ruble">Ruble</option>
+			<option value="baht">BAHT</option>
+
+			
 		</select>
 
 		<label for="to">To</label>
 		<select name="to">
+
+		    <option value="usd">USD</option>
 			<option value="euro">EURO</option>
 			<option value="baht">BAHT</option>
+			<option value="ruble">Ruble</option>
+			<option value="pound">POUND</option>
 		</select>
 
 		<label for="c_input">Enter Amount</label>
 		<input type="number" name="camount" required=""/>
 
 		
-		<button type="submit" name="convert" value="Convert" class="btn btn-success">Change</button>
+		<button type="submit" name="convert" value="Convert" class="btn btn-success"><b>Submit</b></button>
 	</form>
 	<br /><br /><br />
 
+<div class='btn btn-success center' style="margin-left: 30%;font-weight:bold">
+	
+	<h4> Result: </h4>
 
 <?php 
+
 	if (isset($_POST['convert'])) {
 		$from=$_POST['from'];
 		$to=$_POST['to'];
@@ -61,29 +74,101 @@
 			exit();
 		}
 
-		echo '<div class="center">';
+	
 		if($from=='usd'){
 			if($to=='euro'){
 				$result=$amount*0.88;
-				echo "Exchange is ".$result." euro";
+				echo $result." Euro";
 			}
 			else if ($to=='baht') {
 				$result=$amount*34.82;
-				echo "Exchange is ".$result." baht";
+				echo $result." Baht";
+			}
+				else if ($to=='ruble') {
+				$result=$amount*55.82;
+				echo $result." Ruble";
+			}
+				else if ($to=='pound') {
+				$result=$amount / 1.82;
+				echo $result." Round";
 			}
 		}
 		else if ($from=='pound') {
 			if($to=='euro'){
 				$result=$amount*1.27;
-				echo "Exchange is ".$result." euro";
+				echo $result." Euro";
 			}
 			else if ($to=='baht') {
 				$result=$amount*50.13;
-				echo "Exchange is ".$result." baht";
+				echo $result." Raht";
+			}
+			else if ($to=='ruble') {
+				$result=$amount*65.82;
+				echo $result." Ruble";
+			}
+			else if ($to=='usd') {
+				$result=$amount*5.82;
+				echo $result." Ruble";
 			}
 		}
-		echo '</div>';
+		else if ($from=='ruble') {
+			if($to=='euro'){
+				$result=$amount /  1.27;
+				echo $result." Euro";
+			}
+			else if ($to=='baht') {
+				$result=$amount*5.13;
+				echo $result." Baht";
+			}
+			else if ($to=='usd') {
+				$result=$amount /  65.82;
+				echo $result." Ruble";
+			}
+				else if ($to=='pound') {
+				$result=$amount / 5.82;
+				echo $result." Pound";
+			}
+		}
+			if($from=='baht'){
+			if($to=='euro'){
+				$result=$amount / 45.88;
+				echo $result." euro";
+			}
+			else if ($to=='usd') {
+				$result=$amount / 34.82;
+				echo $result." Baht";
+			}
+				else if ($to=='ruble') {
+				$result=$amount / 5.82;
+				echo $result." Ruble";
+			}
+				else if ($to=='pound') {
+				$result=$amount / 65.82;
+				echo $result." Pound";
+			}
+		}
+			if($from=='euro'){
+			if($to=='baht'){
+				$result=$amount * 45.88;
+				echo $result." Euro";
+			}
+			else if ($to=='usd') {
+				$result=$amount * 1.82;
+				echo $result." Baht";
+			}
+				else if ($to=='ruble') {
+				$result=$amount / 20.82;
+				echo $result." Buble";
+			}
+				else if ($to=='pound') {
+				$result=$amount / 5.82;
+				echo $result." Pound";
+			}
+		}
+	
 	}
+
  ?>
 		
-
+</div>
+</div>
