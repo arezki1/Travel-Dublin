b2a1314b9a8004c2051b940b63139e299f3adebe
maurams <maurice.ramsbottom@gmail.com>
2016-04-20 20:43:04 +0000

    added code references

--- registration/login.php
+++ registration/login.php
@@ -1,14 +1,15 @@
+<!--http://forums.devshed.com/php-faqs-stickies-167/program-basic-secure-login-system-using-php-mysql-891201.html -->
+<!--http://www.formget.com/login-form-in-php-->
 <?php 
 
     // First we execute our common code to connection to the database and start the session 
     require("common.php"); 
      
     // This variable will be used to re-display the user's username to them in the 
-    // login form if they fail to enter the correct password.  It is initialized here 
-    // to an empty value, which will be shown if the user has not submitted the form. 
+    // login form if they fail to enter the correct password.  
     $submitted_username = ''; 
      
-    // This if statement checks to determine whether the login form has been submitted 
+    // check to determine whether the login form has been submitted 
     // If it has, then the login code is run, otherwise the form is displayed 
     if(!empty($_POST)) 
     { 
@@ -44,8 +45,7 @@
         } 
          
         // This variable tells us whether the user has successfully logged in or not. 
-        // We initialize it to false, assuming they have not. 
-        // If we determine that they have entered the right details, then we switch it to true. 
+        // We initialize it to false, assuming they have not.
         $login_ok = false; 
          
         // Retrieve the user data from the database.  If $row is false, then the username 
@@ -53,9 +53,7 @@
         $row = $stmt->fetch(); 
         if($row) 
         { 
-            // Using the password submitted by the user and the salt stored in the database, 
-            // we now check to see whether the passwords match by hashing the submitted password 
-            // and comparing it to the hashed version already stored in the database. 
+            // Check if passwords match with hash and salt
             $check_password = hash('sha256', $_POST['password'] . $row['salt']); 
             for($round = 0; $round < 65536; $round++) 
             { 
